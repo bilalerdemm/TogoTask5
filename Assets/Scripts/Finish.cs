3414d5e773ee7f45bf4bb8e28a8342f1e834f365
bilalerdemm <bilalerdemm96@gmail.com>
2023-01-08 15:52:12 +0300

    Finish mekaniği oluşturuldu, cube ve sphire ayırt edilebiliyor

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -6,11 +6,11 @@ public class Finish : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Collected") && PlayerGameController.instance.isCube)
+        if (other.gameObject.CompareTag("Collected") && other.gameObject.GetComponent<MeshRenderer>().enabled == true)
         {
             Debug.Log("Finishe cube geldi");
         }
-        else if (other.gameObject.CompareTag("Collected") && !PlayerGameController.instance.isCube)
+        else if (other.gameObject.CompareTag("Collected") && other.gameObject.GetComponent<MeshRenderer>().enabled == false)
         {
             Debug.Log("Finishe sphire geldi");
         }
